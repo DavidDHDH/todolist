2f103978312c95c6e4d6e286ab7ef282ea65e62f
DavidDHDH <david.heimfert@gmail.com>
2023-03-16 10:13:02 -0400

    version final

---
+++ script.js
@@ -0,0 +1,20 @@
+import { Todolist } from './components/Todolist.js'
+import {fetchJSON} from './functions/api.js'
+import { createElement } from './functions/dom.js'
+
+
+try {
+    const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    const list = new Todolist(todos)
+    list.appendTo(document.querySelector('#todolist'))
+} catch (error) {
+    const alertElement = createElement('div', {
+        class: "alert alert-danger m-2",
+        role: "alert"
+    })
+    alertElement.innerText = "Impossible de charger les élements"
+    document.body.prepend(alertElement)
+    console.log(error)
+}
+
+
